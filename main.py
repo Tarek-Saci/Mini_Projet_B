a7438db5a14cd0b0a7e8f830ca7d3dfa2d5835ea
Tarek-Saci <tarek7saci@gmail.com>
2023-05-31 16:06:54 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#-------fixer les parametres----------------------------------------------------
+p1 = 1
+p2 = 1
+p3 = 1
+p4 = 1
+n = 15
+#-------une fonction qui determine la solution analytique ----------------------
+def integrale_analytique(p1 , p2 , p3 , p4 , a , b):
+
+    primitive = (p1 * x) + (0.5 * p2 * x**2) + ((1/3) * p3 * x**3) + (0.25 * p4 * x**4)
+
+
+    return
+
+#-------fonction methode des rectangles-----------------------------------------

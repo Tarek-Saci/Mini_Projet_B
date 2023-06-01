cd831bc56145c01f11021bfc271f9bbe78d07fee
Tarek-Saci <tarek7saci@gmail.com>
2023-06-01 16:31:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-#-------fixer les parametres----------------------------------------------------
+# -------fixer les parametres----------------------------------------------------
 p1 = 1
-p2 = 1
-p3 = 1
-p4 = 1
+p2 = 2
+p3 = 3
+p4 = 4
 n = 15
-#-------une fonction qui determine la solution analytique ----------------------
-def integrale_analytique(p1 , p2 , p3 , p4 , a , b):
+a = -10
+b = 15
 
-    primitive = (p1 * x) + (0.5 * p2 * x**2) + ((1/3) * p3 * x**3) + (0.25 * p4 * x**4)
+# -------une fonction qui determine la solution analytique ----------------------
+def integrale_analytique(p1, p2, p3, p4, a, b):
+    x = a
+    primitive_1 = (p1 * x) + (0.5 * p2 * x ** 2) + ((1 / 3) * p3 * x ** 3) + (0.25 * p4 * x ** 4)
+    x = b
+    primitive_2 = (p1 * x) + (0.5 * p2 * x ** 2) + ((1 / 3) * p3 * x ** 3) + (0.25 * p4 * x ** 4)
+    resultat = primitive_2 - primitive_1
+    return resultat
 
+# -------fonction methode des rectangles-----------------------------------------
 
-    return
-
-#-------fonction methode des rectangles-----------------------------------------
+print(integrale_analytique(p1,p2,p3,p4,a,b))

3e9621021ea20e5037d71c6b9b3a2de4d7395e06
Tarek-Saci <tarek7saci@gmail.com>
2023-06-01 17:48:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ p1 = 1
 p2 = 2
 p3 = 3
 p4 = 4
-n = 10
+n = 20
 a = -10
 b = 15
 
@@ -21,11 +21,16 @@ def integrale_analytique(p1, p2, p3, p4, a, b):
 # -------fonction methode des rectangles-----------------------------------------
 def integrale_rectangles(p1, p2, p3, p4, a, b, n):
     largeur_rectangle = (b - a) / n
-    x = a
-    while x <= b:
-        print(x)
+    liste_aires_rectangle = [] #on initalise une liste qui va contenir toutes les aires des rectangles qu'on poura ensuite aditionner
+    x = a + largeur_rectangle / 2  # on éssaye de placer x au milieu du rectangle
+    while x <= b:  # on fait varier x avec un pas égale a la largeur du rectangle
+        hauteur_rectagle = (p1) + (p2 * x) + (p3 * x ** 2) + (p4 * x ** 3)
+        aire = hauteur_rectagle * largeur_rectangle
+        liste_aires_rectangle.append(aire)
         x += largeur_rectangle
-    return
+    resultat = sum(liste_aires_rectangle)
+    print(resultat)
+    return resultat
 
 
 # --------------------------------------------------------------------------------
